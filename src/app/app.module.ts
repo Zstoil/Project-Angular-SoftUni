c463b889f0a7300552ab7dd73e07cf453655a61d
Zstoil <113816988+Zstoil@users.noreply.github.com>
2023-07-31 19:46:33 +0300

    up

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
+import { MainComponent } from './main/main.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent, 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    UserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
